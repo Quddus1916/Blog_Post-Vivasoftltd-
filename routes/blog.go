2d09152b7d9e7138053e5c4a0ac94d87492e9d6a
Quddus1916 <quddusjunior1916@gmail.com>
2022-05-31 07:49:59 -0500

    restful api best practices

--- routes/blog.go
+++ routes/blog.go
@@ -11,11 +11,11 @@ func Blogroutes(e *echo.Echo) {
 	blog.Use(middlewares.Authenticate)
 
 	blog.POST("/post", controllers.Create_blog)
-	blog.GET("/by_category/:category_name", controllers.Get_blog_by_category)
+	blog.GET("/by-category/:category_name", controllers.Get_blog_by_category)
 	blog.GET("/all_blogs", controllers.Get_all)
 	blog.PATCH("/update/:id", controllers.Update_blog)
 	blog.DELETE("/delete/:id", controllers.Delete_blog)
 	blog.POST("/comment", controllers.Post_comment)
-	blog.POST("/create_category", controllers.Create_category)
+	blog.POST("/create-category", controllers.Create_category)
 
 }
